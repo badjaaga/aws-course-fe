1f8cb750ad0c5b9c10cf9113a28b4efce5b81c82
Nadzeya_Molchan <badjaaga@gmail.com>
2023-03-14 00:26:55 +0100

    feat: create import service integration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,14 +9,14 @@ export const environment: Config = {
   apiEndpoints: {
     product: 'https://3wk8qxh4g4.execute-api.eu-west-1.amazonaws.com/dev/products',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    import: "https://ufbk6obmeb.execute-api.eu-west-1.amazonaws.com/dev",
     bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
     product: true,
     order: false,
-    import: false,
+    import: true,
     bff: true,
     cart: false,
   },
